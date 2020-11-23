d9c3e8d6b7c3751befeea97ec8370a18dc4a4a2d
DiamondTermite <hardik.m.negi@gmail.com>
2020-11-23 17:00:40 -0500

    foodlord

--- sketch.js
+++ sketch.js
@@ -1,22 +1,42 @@
-//Create variables here
-
+var database;
+var foodCount,dogsprite,dogImg1,dogImg2
 function preload()
 {
-	//load images here
+dogImg1=loadImage("images/dogImg.png");
+dogImg2=loadImage("images/dogImg.png");
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1000, 700);
   
+  database=firebase.database()
+  var locationOfChild = database.ref("food")
+  locationOfChild.on("value",readFoodCount,showError)
 }
 
 
 function draw() {  
-
+background("red")
   drawSprites();
-  //add styles here
-
+  if(keyDown(UP_ARROW)){
+updateFoodCount();
+  }
+  fill(255);
+  textSize(20);
+text("foodcoumt ="+foodCount,500,350)
 }
-
-
+function readFoodCount(data){
+  foodCount=data.val()
+}
+function showError(){
+  console.log("there is an error");
+}
+function updateFoodCount(){
+if(foodCount>0){
+  foodCount-=1;
+database.ref("/").set({
+  food:foodCount
+})
+}
+}
 
